4c37e539d5dbb02b3c7de5a4df21af63bb5b8cde
Attila Bertalan 22011516 <115985119+Attila-Bertalan@users.noreply.github.com>
2023-04-17 14:24:09 +0100

    Update StudentResources.php

--- webDevYear2/StudentResources.php
+++ webDevYear2/StudentResources.php
@@ -66,11 +66,11 @@ include "FilesManaging.php";
             <tbody>
             <?php foreach ($files as $file): ?>
                 <tr>
-                <td><?php echo $file['id']; ?></td>
+                <td><?php echo $file['ID']; ?></td>
                 <td><?php echo $file['name']; ?></td>
                 <td><?php echo floor($file['size'] / 1000) . ' KB'; ?></td>
                 <td><?php echo $file['downloads']; ?></td>
-                <td><a href="downloads.php?file_id=<?php echo $file['id'] ?>">Download</a></td>
+                <td><a href="StudentResources.php?ID=<?php echo $file['ID'] ?>">Download</a></td>
                 </tr>
             <?php endforeach;?>
 
