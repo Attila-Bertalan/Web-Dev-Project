ec7414fb4ed567484becdc3f0b80a7c1a4a82096
21005199 <21005199@hope.ac.uk>
2023-05-17 21:15:07 +0100

    Update class_profile.php

--- webDevYear2/class_profile.php
+++ webDevYear2/class_profile.php
@@ -15,20 +15,41 @@
         $name=$_GET['name'];
         $conn = mysqli_connect("localhost", "root", "root", "acetraining");
         $sql="SELECT course_name FROM courses";
-        $classes=mysqli_query($conn,$sql);
+        $classed=mysqli_query($conn,$sql);
+        while ($temp = mysqli_fetch_assoc($classed)) {
+            $classes[]=$temp;
+        }
         $class_length=count($classes);
         $sql="SELECT course_ID FROM courses WHERE course_name='$class'";
         $classId=mysqli_query($conn,$sql);
         $sql="SELECT user_ID FROM enrollment WHERE course_ID='$classId'";
-        $id=mysqli_query($conn,$sql);
+        $result=mysqli_query($conn,$sql);
+        while ($ids = mysqli_fetch_assoc($result)) {
+            $id[]=$ids;
+        }
         $idLength=count($id);
         $students=array();
         for ($i=0;$i<$idLength;$i++) {
-            $sql="SELECT firstname FROM users WHERE user_ID='$id[$i]'";
+            $sql="SELECT firstname FROM users WHERE ID='$id[$i]'";
             $studentFirstname=mysqli_query($conn,$sql);
-            $sql="SELECT lastname FROM users WHERE user_ID='$id[$i]'";
+            $sql="SELECT lastname FROM users WHERE ID='$id[$i]'";
             $studentLastname=mysqli_query($conn,$sql);
             array_push($students,$studentFirstname." ".$studentLastname);
+        }
+    
+        $sql="SELECT user_ID FROM enrollment WHERE course_ID='$classId' AND status='pending'";
+        $result=mysqli_query($conn,$sql);
+        while ($ids = mysqli_fetch_assoc($result)) {
+            $enrollmetnId[]=$ids;
+        }
+        $enrollmetIdLength=count($id);
+        $enrollmentStudents=array();
+        for ($i=0;$i<$enrollmetIdLength;$i++) {
+            $sql="SELECT firstname FROM users WHERE ID='$enrollmetnId[$i]'";
+            $studentFirstname=mysqli_query($conn,$sql);
+            $sql="SELECT lastname FROM users WHERE ID='$enrollmetnId[$i]'";
+            $studentLastname=mysqli_query($conn,$sql);
+            array_push($enrollmentStudents,$studentFirstname." ".$studentLastname);
         }
     ?>
     <div class = "hero">
@@ -66,13 +87,13 @@
             <h3>Notifications:<button type="button" class="close" onclick="closeForm()"><img class="c" src="images/Grey_close_x.svg.png" width="10" height="10"></button></h3>
         
             <?php
-                for ($i = 0; $i<$idLength; $i++) {
+                for ($i = 0; $i<$enrollmetIdLength; $i++) {
                     echo "
                         <div class='notif' id='$i'>
                             <img class='profile' src='images/profileIcon.png' alt='Avatar' style='width:100%'>
                             <button class='btn' onclick='accept($i)'><img src='images/tick.png'></button>
                             <button class='btn' onclick='reject($i)'><img src='images/cross.png'></button>
-                            <p>$students[$i]</p>
+                            <p>$enrollmentStudents[$i]</p>
                         </div>";
                 }
             ?>
@@ -81,7 +102,7 @@
 
     <div class="student_list">
         <?php
-        for ($i = 0; $i<$students_length; $i++) {
+        for ($i = 0; $i<$idLength; $i++) {
             echo "
                 <div class='student'>
                     <img src='images/profileIcon.png' alt='Avatar' style='width:100%'>
@@ -120,6 +141,34 @@
 
     function closeClasses() {
         document.getElementById("classes").style.display = "none";
+    }
+    
+    function openForm() {
+        document.getElementById("notifications").style.display = "block";
+        event.preventDefault();
+    }
+
+    function closeForm() {
+        document.getElementById("notifications").style.display = "none";
+        event.preventDefault();
+    }
+    
+    function accept(num) {
+         <?php
+             $sql="UPDATE enrollments SET status = 'authorized' WHERE user_ID = $enrollmentId[$i]"; 
+             mysqli_query($conn,$sql)
+         ?>
+         document.getElementById(num).style.display = "none";
+         event.preventDefault();
+    }
+
+    function reject(num) {
+         <?php
+            $sql="UPDATE enrollments SET status = 'rejected' WHERE user_ID='$enrollmentId[$i]']"; 
+            mysqli_query($conn,$sql)
+         ?>
+         document.getElementById(num).style.display = "none";
+         event.preventDefault();
     }
 
 </script>
